b6bbdf0d0ef14ccbaefe6eb7c68027c437156ebb
Ramin Hashemi <raminhashemi@Ramins-MacBook-Pro.local>
2021-06-17 17:25:21 +0430

    First uploading

---
+++ script.js
@@ -0,0 +1,46 @@
+var user = {
+    
+    username: "Nimar",
+    password: "123456",
+}
+
+alert("Make your Profile!");
+var age = prompt("What is your Age?");
+function checkAge() {
+    if (Number(age) < 18) {
+        alert("Sorry, you are too young to make profile. Powering off");
+        return "Sorry, you are too young to make profile. Powering off";
+    }
+        else if (Number(userAge) === 18) {
+        alert("Congratulations on your first make profile. Enjoy the ride!");
+        return "Congratulations on your first make profile. Enjoy the ride!";
+        }
+        
+        else {
+        alert("Powering On. Enjoy the ride!");
+        return "Powering On. Enjoy the ride!";
+        }
+}    
+
+checkDriverAge();
+
+var username = prompt("What is your Username?");
+var password = prompt("What is your Password?");
+
+var dataBase = [
+    {
+        username: user.username,
+        password: user.password,
+    },
+];
+var newsFeed = [
+    {
+        username: user.username,
+        timeline: "Introducing Management Engineering",
+    },
+    
+    {
+        username: "Suzi",
+        timeline: "How Does it Works?",
+    },
+];
